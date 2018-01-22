3c91e53e706e6a4c7d2c411d9e9552c93b3a09fe
Harris Brakmic <brakmic@gmail.com>
2018-01-22 11:38:22 +0100

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,13 +3,18 @@
 [ -d .git ] && [ -f ./tor/.git ] || \
   { echo "Please run this command from the root of a recursive Git clone of the DeepOnion repository." && exit 1; }
 
-pushd tor && git checkout -f . && git clean -fdx . && popd
-git submodule update --init
+git submodule init 
+git submodule sync --recursive 
+git submodule update --recursive --force
+
+pushd tor && git clean -fdx . && popd
 
 autoreconf --no-recursive --install
 
+PATCH="patch --no-backup-if-mismatch -f"
+
 pushd tor
-patch --no-backup-if-mismatch -f -p0 < ../tor-or-am.patch
-patch --no-backup-if-mismatch -f -p0 < ../tor-am.patch
+$PATCH -p0 < ../tor-or-am.patch
+$PATCH -p0 < ../tor-am.patch
 ./autogen.sh
 popd
