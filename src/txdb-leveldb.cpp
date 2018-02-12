b3f5bbdba632ccc494fd12ecf74ad1a4a9c2a8c3
Harris Brakmic <brakmic@gmail.com>
2018-02-12 21:44:35 +0100

    debug.log outputs

--- src/txdb-leveldb.cpp
+++ src/txdb-leveldb.cpp
@@ -430,7 +430,7 @@ bool CTxDB::LoadBlockIndex()
     bnBestChainTrust = pindexBest->bnChainTrust;
 
     printf("LoadBlockIndex(): hashBestChain=%s  height=%d  trust=%s  date=%s\n",
-      hashBestChain.ToString().substr(0,20).c_str(), nBestHeight, CBigNum(bnBestChainTrust).ToString().c_str(),
+      hashBestChain.ToString().substr(0,20).c_str(), nBestHeight, bnBestChainTrust.ToString().c_str(),
       DateTimeStrFormat("%x %H:%M:%S", pindexBest->GetBlockTime()).c_str());
 
     // NovaCoin: load hashSyncCheckpoint
