3b2c440059b9f2fb110c89d8dc4126356cff1c3a
deeponion <rs1953@yahoo.com>
2018-04-28 18:43:20 -0700

    prepare 1.7.0 release

--- src/version.cpp
+++ src/version.cpp
@@ -41,10 +41,10 @@ const std::string CLIENT_NAME("DeepOnion");
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "" commit
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-CANDIDATE" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj,min,rev,build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) ""
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-CANDIDATE"
 
 #ifndef BUILD_DESC
 #    ifdef GIT_COMMIT_ID
