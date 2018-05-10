c27e09c049d5da5ba39f47ee40c2543adbdda2ca
deeponion <rs1953@yahoo.com>
2018-05-10 12:47:10 -0700

    bump protocol version

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int DATABASE_VERSION = 70508;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 80011;
+static const int PROTOCOL_VERSION = 80012;
 
 static const int MIN_PROTO_VERSION = 80010;
 static const int MIN_PROTO_VERSION_AFTER_SWITCH = 80011;
